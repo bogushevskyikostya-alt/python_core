196e00f7c42bdf26512604e813c526aeaac08175
Kostya <bogushevskyikostya@gmail.com>
2026-01-18 14:49:54 +0200

    переробив класну роботу

--- hw/classwork.py
+++ hw/classwork.py
@@ -11,7 +11,7 @@ elif score >= 50:
 else:
     print("Незадовільно")
 
-    salary = float(input("Введіть заробітну плату: "))
+salary = float(input("Введіть заробітну плату: "))
 experience = float(input("Введіть стаж роботи (в роках): "))
 
 if experience < 1:
@@ -22,42 +22,37 @@ elif experience < 5:
     print("Премія:", salary * 0.10)
 else:
     print("Премія:", salary * 0.15)
+num = input("Введіть чотиризначне число: ")
 
-    num = int(input("Введіть чотиризначне число: "))
-
-num_abs = abs(num)
-
-if num_abs < 1000 or num_abs > 9999:
+if len(num) != 4 or not num.isdigit():
     print("Помилка: число не чотиризначне")
 else:
-    s = 0
-    for digit in str(num_abs):
-        s += int(digit)
+    s = int(num[0]) + int(num[1]) + int(num[2]) + int(num[3])
 
-    if s % 2 == 0:
-        print("Сума цифр парна")
-    else:
-        print("Сума цифр непарна")
+if s % 2 == 0:  
+    print("Сума цифр парна")  
+else:  
+    print("Сума цифр непарна")
 
-        num = input("Введіть шестизначне число: ")
+num = input("Введіть шестизначне число: ")
 
 if len(num) != 6 or not num.isdigit():
     print("Помилка: число не шестизначне")
 else:
-    first_sum = int(num[0]) + int(num[1]) + int(num[2])
-    second_sum = int(num[3]) + int(num[4]) + int(num[5])
+    s1 = int(num[0]) + int(num[1]) + int(num[2])
+s2 = int(num[3]) + int(num[4]) + int(num[5])
 
-    if first_sum == second_sum:
-        print("Число щасливе")
-    else:
-        print("Число нещасливе")
+if s1 == s2:  
+    print("Число щасливе")  
+else:  
+    print("Число нещасливе")
 
-        num = input("Введіть шестизначне число: ")
+num = input("Введіть шестизначне число: ")
 
 if len(num) != 6 or not num.isdigit():
     print("Помилка: число не шестизначне")
 else:
     new_num = num[5] + num[4] + num[2] + num[3] + num[1] + num[0]
-    print("Результат:", new_num)
-
-    
+print("Результат:", new_num)
+print("Задовільно")
+print("Незадовільно")
